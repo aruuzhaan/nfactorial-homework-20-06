8da0e66a94b1db72e47dd500159c341d45d09649
Сибекова Аружан Ринатовна <aruzhan.sibekova@nu.edu.kz>
2023-02-10 01:07:30 +0600

    events

---
+++ index.js
@@ -0,0 +1,32 @@
+
+//TASK NUMBER ONE STARTS HERE
+// let thetext = document.getElementById('text');
+// console.log(thetext);
+
+// const btn = document.querySelector('#hider');
+// console.log(btn);
+
+// const removetext = function(event){
+//     thetext.hidden = true;
+//     btn.removeEventListener('click', removetext);
+// }
+
+// btn.addEventListener('click', removetext);
+
+//TASK NUMBER TWO STARTS HERE
+// button.addEventListener("click", () => alert("1"));
+
+// button.removeEventListener("click", () => alert("1"));
+
+// button.onclick = () => alert(2);
+
+//the answer is : 1 and 2 due to the difference of the first two functions.
+
+//THIRD TASK STARTS HERE
+// let btnx = document.querySelectorAll('.pane');
+
+// for(let pane of btnx) {
+//     pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+//     pane.firstChild.onclick = () => pane.remove();
+//   }
+
